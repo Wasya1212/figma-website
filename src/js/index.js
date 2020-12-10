ca853245d8dae4709fe1f7e2be54294d760e19ed
Wasya1212 <leonardo.1212@yandex.ua>
2020-12-10 15:09:50 +0200

    added portfolio section

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,37 @@
 require('../libs/splide/dist/js/splide.min.js');
 require('../libs/splide/dist/css/splide.min.css');
 
-const sliderOpts = {
+const primarySliderOpts = {
   type: 'fade',
   autoplay: true,
   drag: false,
   arrows: true,
   speed: 2000,
   rewind: true,
-  pagination: false,
+  pagination: true,
   pauseOnHover: false
 };
 
+const secondarySliderOpts = {
+	rewind      : true,
+	fixedWidth  : 102,
+	fixedHeight : 105,
+	isNavigation: true,
+	gap         : 10,
+	focus       : 'center',
+	pagination  : false,
+	cover       : true,
+	breakpoints : {
+		'600': {
+			fixedWidth  : 66,
+			fixedHeight : 40,
+		}
+	}
+};
+
 document.addEventListener( 'DOMContentLoaded', function () {
-  const slider = new Splide('.splide', sliderOpts).mount();
+  const primarySlider  = new Splide('.splide', primarySliderOpts);
+  const secondarySlider = new Splide( '#primary-slider', secondarySliderOpts).mount();
+  
+  primarySlider.sync( secondarySlider ).mount();
 } );
